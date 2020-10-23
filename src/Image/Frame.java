11e711a68a260128d3eaab37ae8a96c2ee8a3b89
Jędrzej Zięcina <jedrzejziecina@gmail.com>
2020-10-23 13:37:37 +0200

    Added option to display image

--- src/Image/Frame.java
+++ src/Image/Frame.java
@@ -1,5 +1,47 @@
 package Image;
 
-public class Frame {
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
 
+public class Frame extends JFrame{
+
+            public Frame(){
+                int width = Toolkit.getDefaultToolkit().getScreenSize().width;
+                int height = Toolkit.getDefaultToolkit().getScreenSize().height;
+
+                this.setSize(width, height);
+                int frameWidth = this.getSize().width;
+                int frameHeight = this.getSize().height;
+                this.setLocation((width-frameWidth)/2, (height-frameHeight)/2);
+
+                JLabel label = new JLabel();
+                add(label);
+
+                JFileChooser option = new JFileChooser();
+                option.setCurrentDirectory(new File("."));
+
+                JMenuBar menuBar = new JMenuBar();
+                setJMenuBar(menuBar);
+
+                JMenu menu = new JMenu("File");
+                menuBar.add(menu);
+
+                JMenuItem open = new JMenuItem("Open");
+                menu.add(open);
+                open.addActionListener(event -> {
+                    int result = option.showOpenDialog(null);
+
+                    if(result == JFileChooser.APPROVE_OPTION){
+                        String name = option.getSelectedFile().getPath();
+                        label.setIcon(new ImageIcon(name));
+                    }
+                        }
+                );
+
+                setTitle("Image Viewer");
+                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                setVisible(true);
+
+            }
 }
--- src/Image/Main.java
+++ src/Image/Main.java
@@ -5,11 +5,6 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
-
-        EventQueue.invokeLater(() -> {
-            var frame = new Frame();
-
-        });
-
+        EventQueue.invokeLater(() -> new Frame());
     }
 }
